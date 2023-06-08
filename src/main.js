6601764354e710c4cb8f5eeef00f73534e1bc60f
michaelmehr <michaelmehr0@gmail.com>
2023-06-07 19:27:59 -0500

    operate function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+let firstNum, secondNum, operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +14,19 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case 'add':
+            return add(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+        case 'multiply':
+            return multiply(num1, num2);
+        case 'divide':
+            return divide(num1, num2);
+        default:
+            return null;
+    }
 }
